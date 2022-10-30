092c3cc753013a8edae14c7e9514b8dd1fb841a7
Amal221200 <amalmurikkoli2000@gmail.com>
2022-10-30 12:27:26 +0530

    for loop happy numbers

---
+++ practical/loops/02_for_happy_numbers.py
@@ -0,0 +1,18 @@
+def squareSum(num):
+    digits = len(str(num))
+    sum=0
+    for i in range(0,digits):
+        sum += int(str(num)[i])**2
+    return sum
+
+for num in range(1, 101):
+    n = num    
+    while n != 1 or n != 7:
+        n = squareSum(n)
+        if n==1 or n==7:
+            print(num)
+            break
+        elif n < 10:
+            break
+        else:
+            continue
